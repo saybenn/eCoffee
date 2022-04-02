6ec25fe52e25a17627f33a87caa63df49f0d7300
Elliott Chambers <elijr1980@gmail.com>
2022-04-02 00:16:08 -0400

    homepage

--- backend/server.js
+++ backend/server.js
@@ -213,9 +213,9 @@ app.post("/create-payment-intent", async (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
   app.get("/", (req, res) => {
     res.send("API is running");
